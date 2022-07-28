249b991921c468f3d1a3dbb4b9394d7bfd370b59
ponbac <root@ponbac.xyz>
2022-07-28 23:02:25 +0200

    added welcome section

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Navbar = () => {
+const TopSection = () => {
   const NavButton = (props: { text: string; to: string }) => {
     return (
       <Link href={props.to}>
@@ -15,53 +15,90 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="">
-      <div className="flex flex-col lg:flex-row items-center justify-between px-24">
-        <Link href="/">
-          <a>
+    <div className="flex flex-col lg:flex-row items-center justify-between px-24">
+      <Link href="/">
+        <a>
+          <Image
+            className=""
+            src="/images/nrh-logo.png"
+            alt="Swish QR kod (9005323)"
+            width="230"
+            height="146"
+            layout="fixed"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center lg:items-end gap-4 py-4 lg:py-0">
+        <Link href="http://www.insamlingskontroll.se/">
+          <a target="_blank">
             <Image
               className=""
-              src="/images/nrh-logo.png"
+              src="/images/insamlingskontroll.png"
               alt="Swish QR kod (9005323)"
-              width="230"
-              height="146"
+              width="121"
+              height="30"
               layout="fixed"
             />
           </a>
         </Link>
-        <div className="flex flex-col items-center lg:items-end gap-4">
-          <Link href="http://www.insamlingskontroll.se/">
-            <a target="_blank">
-              <Image
-                className=""
-                src="/images/insamlingskontroll.png"
-                alt="Swish QR kod (9005323)"
-                width="121"
-                height="30"
-                layout="fixed"
-              />
-            </a>
-          </Link>
-          <div className="flex flex-col lg:flex-row gap-2">
-            <NavButton
-              text="Enkät"
-              to="https://nrhtrauma.se/nrh-trauma-enkatundersokning-survey-september-2020/"
-            />
-            <NavButton
-              text="Bli medlem i föreningen"
-              to="https://nrhtrauma.se/nrh-trauma-enkatundersokning-survey-september-2020/"
-            />
-            <NavButton
-              text="Dela din berättelse"
-              to="https://docs.google.com/forms/d/e/1FAIpQLSfEeZU3x7hBhrRIHLQpRYm19ck_e80nv3eQXp2iNGfxoNHOFg/viewform"
-            />
-            <NavButton
-              text="Skänk en gåva"
-              to="https://www.nrhtrauma.se/skank-en-gava/"
-            />
-          </div>
+        <div className="flex flex-col lg:flex-row gap-2">
+          <NavButton
+            text="Enkät"
+            to="https://nrhtrauma.se/nrh-trauma-enkatundersokning-survey-september-2020/"
+          />
+          <NavButton
+            text="Bli medlem i föreningen"
+            to="https://nrhtrauma.se/nrh-trauma-enkatundersokning-survey-september-2020/"
+          />
+          <NavButton
+            text="Dela din berättelse"
+            to="https://docs.google.com/forms/d/e/1FAIpQLSfEeZU3x7hBhrRIHLQpRYm19ck_e80nv3eQXp2iNGfxoNHOFg/viewform"
+          />
+          <NavButton
+            text="Skänk en gåva"
+            to="https://www.nrhtrauma.se/skank-en-gava/"
+          />
         </div>
       </div>
+    </div>
+  );
+};
+
+const NavMenu = () => {
+  const MenuLink = (props: { to: string; text: string; active?: boolean }) => {
+    return (
+      <Link href={props.to}>
+        <a>
+          <p
+            className={`${
+              props.active ? "text-orange-400" : ""
+            } text-lg font-bold hover:text-orange-400 transition-all`}
+          >
+            {props.text}
+          </p>
+        </a>
+      </Link>
+    );
+  };
+
+  return (
+    <div className="flex flex-col lg:flex-row lg:gap-8 items-center justify-center py-2">
+      <MenuLink to="/" text="Startsida" active={true} />
+      <MenuLink to="/" text="Diagnosticering/Diagnoser" />
+      <MenuLink to="/" text="Kunskapsspridning" />
+      <MenuLink to="/" text="Påverkansarbete" />
+      <MenuLink to="/" text="Stöd oss" />
+      <MenuLink to="/" text="Om oss" />
+      <MenuLink to="/" text="Lokalförening" />
+    </div>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <nav className="divide-y">
+      <TopSection />
+      <NavMenu />
     </nav>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import Head from "next/head";
-import Image from "next/image";
 import { ReactElement } from "react";
 import Layout from "../components/layout";
 import { NextPageWithLayout } from "./_app";
+
+const WelcomeSection = () => {
+  return (
+    <div
+      className="flex flex-col justify-center items-center text-center space-y-8 bg-cover bg-center w-full h-[40rem]"
+      style={{
+        backgroundImage:
+          "url(https://nrhtrauma.se//wp-content/uploads/2019/12/CCJ-Tornn-ligs.jpg)",
+        boxShadow: "inset 0 0 0 1000px rgba(200,200,200,.8)",
+        textShadow: "1px 1px gray",
+      }}
+    >
+      <h2 className="text-4xl font-bold">VÄLKOMMEN TILL</h2>
+      <h1 className="text-6xl font-bold">NRH TRAUMA RIKS</h1>
+      <h3 className="text-2xl">NACK-, RYGG- & HJÄRNSKADADES RÄTT!</h3>
+    </div>
+  );
+};
 
 const Home: NextPageWithLayout = () => {
   return (
@@ -13,7 +30,11 @@ const Home: NextPageWithLayout = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>INDEX</main>
+      <main>
+        <div className="flex flex-col min-w-full justify-center items-center">
+          <WelcomeSection />
+        </div>
+      </main>
     </div>
   );
 };
