30a58cd659829a81a6600b3f7b12b453e6cb096e
ponbac <root@ponbac.xyz>
2022-07-29 16:17:39 +0200

    added custom document

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,7 +10,6 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen pt-6">
-      
       {/* <Navbar /> */}
       <ModernNavbar />
       <main className="flex-1">{children}</main>
